ffa77e88f1d3ca2e5fb1b33518483a8f3a76fa03
Ravil <34988095+RavilBoyukzade@users.noreply.github.com>
2022-12-01 19:16:13 +0300

    add API host and key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,8 @@
 export const environment={
-    production:false
+    production:false,
+    weatherApiBaseUrl: 'https://yahoo-weather5.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName :'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'yahoo-weather5.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '3840d2874emsh6c5246d65073ee5p1a0fecjsn93ecd0619d67'
 };
